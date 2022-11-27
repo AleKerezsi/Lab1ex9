4507eabb05a256811e62d9b3aa4a1bbaec6359dd
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 21:58:07 +0200

    Add project files.

---
+++ Lab1ex9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care interschimba valoarea a doua variabile intregi */
+
+            Console.WriteLine("Introduceti x:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti y:");
+            int y = int.Parse(Console.ReadLine());
+
+            int z;
+
+            z = x;
+            x = y;
+            y = z;
+
+            Console.WriteLine("x= " + x+" "+"y= "+y);   
+          
+        }
+    }
+}
